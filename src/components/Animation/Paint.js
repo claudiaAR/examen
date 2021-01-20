4b3d83c2c842f31ac9709eae225ec245692cb31f
Claudia Aranda <claudia_15_@hotmail.com>
2021-01-20 10:26:58 +0100

    svg anpassning

--- src/components/Animation/Paint.js
+++ src/components/Animation/Paint.js
@@ -13,6 +13,16 @@ function Paint(props) {
           fill="currentColor"
         />
       </svg>
+
+      {/* <svg width="1115" height="717" viewBox="0 0 1115 717" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path className={styles.animated} d="M1114.5 0H46.5H-24L-16 497L-12.5 618.5C3.79863 689.399 80.5892 732.944 153.5 711.5C196 699 232 636.5 232 636.5L404 430.5C449 392 484.5 386 528 394.5C558 400.362 572 409.5 624 476C676 542.5 756.5 554 812 497C867.5 440 1114.5 0 1114.5 0Z" 
+      fill="#FFF491"
+      fill="currentColor"
+    
+      /> */}
+      
+      {/* </svg> */}
+
     </div>
   );
 }
